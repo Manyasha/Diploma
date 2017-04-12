801f15da17ec9ec921f561548cf17e0414f0a9dd
Marina Tsymbal <marina.tsymbal@filmon.com>
2017-04-12 11:12:48 +0300

    test

--- diploma.py
+++ diploma.py
@@ -1,35 +1,48 @@
 from sympy import diff, symbols, Symbol, cos, sin
 import numpy as np
-from scipy import optimize
+from scipy.optimize import minimize
+from sympy.utilities.lambdify import lambdify
+import sympy as sy
 
-x, y = symbols('x y')
-r = diff(cos(x) + 1j*sin(y), x)
-g = diff(cos(x) + 1j*sin(y), y)
-print(r)
-print(g)
-def f(x):
-    return x**3 + x**2
-print(type(f(x)))
-k = diff(f(x),x)
-print(k)
+#sy.init_printing()  # LaTeX like pretty printing for IPython
 
-print(k.subs({x:1}).n())
+x1, x2, x3, x4 = sy.symbols('x1 x2 x3 x4')
+
+xx = (x1, x2, x3, x4)
+f = 100*(x2 - x1**2)**2 + (1 - x1)**2
+f_n = lambdify(xx, f, modules='numpy')
+
+# Build Jacobian:
+jac_f = [f.diff(x) for x in xx]
+jac_fn = [lambdify(xx, jf, modules='numpy') for jf in jac_f]
 
 
-s1 = 'g'
-s2 = str(1)
-print(s1 + s2)
-x1, x2, x3, x4 = symbols('x1 x2 x3 x4')
+def f_v(zz):
+    """ Helper for receiving vector parameters """
+    return f_n(zz[0], zz[1], zz[2], zz[3])
+
+
+def jac_v(zz):
+    """ Jacobian Helper for receiving vector parameters """
+    return np.array([jfn(zz[0], zz[1], zz[2], zz[3]) for jfn in jac_fn])
+
+
+bnds = ((-1, 1), (-1, 1), (-1, 1), (-1, 1))
+zz0 = np.array([1, 1, 1, 1])
+
+rslts = minimize(f_v, zz0, method='SLSQP', jac=jac_v, bounds=bnds)
+print(rslts)
+
+
 def d():
-    #x1 = symbols('x1')
     return x1**3 + x2**2;
+
 def f1():
     return 100*(x2 - x1**2)**2 + (1 - x1)**2
+
 def gradient(f, x0):
     print('---------')
-    #x1, x2, x3, x4 = symbols('x1 x2 x3 x4')
     x = np.array([x1, x2, x3, x4])
-    #n = x0.size
     n = len(x0)
     grad = np.zeros(n)
     for i in range(n):
@@ -39,7 +52,3 @@ def gradient(f, x0):
 #print(gradient(d(), np.array([2], dtype=float)))
 print(gradient(f1(),{x1: -1.2, x2: 1}))
 
-
-def f(x):
-	return 5*(1-x[0]) + x[1]
-print(optimize.fmin_cg(f, [2,2]))
