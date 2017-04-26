73a96cb72a92daa17af3a349b637d5bfc2a18f8e
Marina Tsymbal <marina.tsymbal@filmon.com>
2017-04-26 18:51:10 +0300

    new gradient function

--- diploma.py
+++ diploma.py
@@ -9,6 +9,7 @@ import sympy as sy
 x1, x2, x3, x4 = sy.symbols('x1 x2 x3 x4')
 
 xx = (x1, x2)
+x_array = (x1, x2, x3, x4)
 f = 100*(x2 - x1**2)**2 + (1 - x1)**2
 f_n = lambdify(xx, f, modules='numpy')
 
@@ -16,6 +17,8 @@ f_n = lambdify(xx, f, modules='numpy')
 jac_f = [f.diff(x) for x in xx]
 jac_fn = [lambdify(xx, jf, modules='numpy') for jf in jac_f]
 
+print(jac_f)
+print(jac_fn)
 
 def f_v(zz):
     """ Helper for receiving vector parameters """
@@ -26,21 +29,17 @@ def jac_v(zz):
     """ Jacobian Helper for receiving vector parameters """
     return np.array([jfn(zz[0], zz[1]) for jfn in jac_fn])
 
-
 bnds = ((-1, 1), (-1, 1))
 zz0 = np.array([0, 0])
 
 rslts = minimize(f_v, zz0, method='SLSQP', jac=jac_v, bounds=bnds)
-res = minimize(f_v, zz0, method='Nelder-Mead')
 res0 = minimize(f_v, zz0, method='Powell')
 res1 = minimize(f_v, zz0)
-res2 = minimize(f_v, zz0, method='CG')
 
 print(rslts.x, rslts.nit)
-print(res.x, res.nit)
 print(res0.x, res0.nit)
 print(res1.x, res1.nit)
-print(res2.x, res2.nit)
+
 
 
 def d():
@@ -58,6 +57,10 @@ def gradient(f, x0):
         grad[i] = diff(f, x[i]).subs(x0).n()
     return grad
 
+def newGradient(f, x0):
+    return [diff(f,x) for x in x_array[0:len(x0)]]
+
 #print(gradient(d(), np.array([2], dtype=float)))
 print(gradient(f1(),{x1: -1.2, x2: 1}))
+print(newGradient(f1(),{x1: -1.2, x2: 1}))
 
