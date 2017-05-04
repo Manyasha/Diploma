f07893aff3c1fd8d97eaafd2df563abda577b5a6
Marina Tsymbal <marina.tsymbal@filmon.com>
2017-05-04 17:23:24 +0300

    entry point

---
+++ main.py
@@ -0,0 +1,5 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Modifications of Conjugate Gradient Method')
+
+parser.add_argument('-n', help='List of functions numbers whitch will be processed')
