5eba47ab697978be3f00e6096c05ca683f04ba89
Manyasha <univers94@ukr.net>
2017-05-30 02:58:14 +0300

    create folders

--- Python/conjugateGradientMethods.py
+++ Python/conjugateGradientMethods.py
@@ -22,7 +22,7 @@ def fourStepsCGM(f, x0, eps):
     x_points = [x_k];
 
     while not sf.BreakCriterion(f, x_k, eps):
-        s_k = np.array(s.get(k, s.get(3))(x_k)).flatten()
+        s_k = np.array(s.get(k, s.get(3))(x_k)).flatten()        
         if k < 3:
             s_last[k] = s_k
         else:
@@ -30,14 +30,50 @@ def fourStepsCGM(f, x0, eps):
             s_last[1] = s_last[2]
             s_last[2] = s_k
         
-        beta_k = sf.findStep(f, x_k, s_k) 
+        beta_k = sf.findStep(f, x_k, s_k)
         x_k1 = x_k + beta_k*s_k
         x_k = x_k1
         k = k + 1
         x_points.append(x_k)
         
-    f_star = sf.f_at_point(f, x_k)
-    return {'x_star': x_k, 'f_star': f_star, 'k': k, 'x_points': x_points}   
+    f_star = sf.f_at_point(f, x_k, True)
+    return {'x_star': x_k, 'f_star': f_star, 'k': k, 'x_points': x_points}
+
+def nonQvafFourStepsCGM(f, x0, eps):
+    k = 0
+    x_k = x0
+    x_k_minus_one = 0
+    s_last = {
+        0: 0,
+        1: 0,
+        2: 0
+    }
+    s = {
+        0: lambda x_k: np.matrix(-sf.Gradient(f, x_k)),
+        1: lambda x_k: -sf.Gradient(f, x_k) + sf.gamma_non_kvad(f, x_k, x_k_minus_one, s_last[0])*s_last[0],
+        2: lambda x_k: -sf.Gradient(f, x_k) + sf.gamma_non_kvad(f, x_k, x_k_minus_one, s_last[1])*s_last[1] + sf.gamma_non_kvad(f, x_k, x_k_minus_one, s_last[0])*s_last[0],
+        3: lambda x_k: -sf.Gradient(f, x_k) + sf.gamma_non_kvad(f, x_k, x_k_minus_one, s_last[2])*s_last[2] + sf.gamma_non_kvad(f, x_k, x_k_minus_one, s_last[1])*s_last[1] + sf.gamma_non_kvad(f, x_k, x_k_minus_one, s_last[0])*s_last[0]
+    }
+    x_points = [x_k];
+
+    while not sf.BreakCriterion(f, x_k, eps):
+        s_k = np.array(s.get(k, s.get(3))(x_k)).flatten()        
+        if k < 3:
+            s_last[k] = s_k
+        else:
+            s_last[0] = s_last[1]
+            s_last[1] = s_last[2]
+            s_last[2] = s_k
+        
+        beta_k = sf.findStep(f, x_k, s_k)
+        x_k1 = x_k + beta_k*s_k
+        x_k_minus_one = x_k
+        x_k = x_k1
+        k = k + 1
+        x_points.append(x_k)
+        
+    f_star = sf.f_at_point(f, x_k, True)
+    return {'x_star': x_k, 'f_star': f_star, 'k': k, 'x_points': x_points}
 
 def threeStepsCGM(f, x0, eps):
     k = 0
@@ -67,7 +103,7 @@ def threeStepsCGM(f, x0, eps):
         k = k + 1
         x_points.append(x_k)
 
-    f_star = sf.f_at_point(f, x_k)   
+    f_star = sf.f_at_point(f, x_k, True)   
     return {'x_star': x_k, 'f_star': f_star, 'k': k, 'x_points': x_points}    
     
     
--- Python/secondaryFunctions.py
+++ Python/secondaryFunctions.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cmath as cm
-#import pylab
-from math import sqrt, pow, log
+from math import sqrt, pow, log10, isinf
 from sympy import diff, symbols, Symbol
 from sympy.solvers import solve
 from sympy.utilities.lambdify import lambdify
@@ -12,7 +11,7 @@ from matplotlib import mlab
 import matplotlib.pyplot as plt
 
 #sy.init_printing()  # LaTeX like pretty printing for IPython
-getcontext().prec = 5
+getcontext().prec = 15
 
 x1, x2, x3, x4 = symbols('x1 x2 x3 x4')
 beta = Symbol('beta', real=True, positive=True)
@@ -25,8 +24,7 @@ def Gradient(f, x):
 
 def Hessian(f, x):
     hessian_f = [[lambdify(x_array[0:len(x)], diff(f,x_i,x_j), modules='numpy') for x_i in x_array[0:len(x)]] for x_j in x_array[0:len(x)]]
-    hessian_fn = [[hessian_f[h_i][h_j](*x) for h_i in range(len(x))] for h_j in range(len(x))]  
-    #print([[diff(f,x_i,x_j) for x_i in x_array[0:len(x)]] for x_j in x_array[0:len(x)]])
+    hessian_fn = [[hessian_f[h_i][h_j](*x) for h_i in range(len(x))] for h_j in range(len(x))]
     return np.matrix(hessian_fn)
 
 def norm(x):
@@ -37,24 +35,41 @@ def norm(x):
     return norm
 
 def BreakCriterion(f, x, eps):
-    print(norm(Gradient(f, x)))
-    return norm(Gradient(f, x)) < eps
+    norm_g = norm(Gradient(f, x))
+    #print(isinf(norm_g), norm_g)
+    return True if isinf(norm_g) else norm_g < eps
 
 def gamma(f, x, s):
     numerator = np.matrix(Gradient(f, x))@(Hessian(f, x)*np.matrix(s).T)
     denominator = np.matrix(s)@(Hessian(f, x)*np.matrix(s).T)
-    return 0 if denominator == 0 else numerator / denominator 
+    return 0 if denominator == 0 else numerator / denominator
+
+def gamma_non_kvad(f, x_k, x_k_minus_one, s):
+    grad = np.matrix(Gradient(f, x_k)) - np.matrix(Gradient(f, x_k_minus_one))
+    if grad > 0:
+        numerator = 0
+        denominator = 0
+    else:
+        numerator = np.matrix(Gradient(f, x_k))@grad
+        denominator = np.matrix(s)@grad
+            
+    return 0 if denominator == 0 else numerator / denominator
 
 def findStep(f, x_k, s_k):
     point = x_k + beta*s_k
     mod_f = lambdify(x_array[0:len(x_k)], f, modules='numpy')
-    mod_fn = lambdify(beta, mod_f(*point), modules='numpy')       
+    mod_fn = lambdify(beta, mod_f(*point), modules='numpy')
     beta_min = minimize_scalar(mod_fn).x
-    return 0 if beta_min < 0 else beta_min
+    if beta_min < 0:
+        print(beta_min)
+    return beta_min if beta_min < 0 else beta_min #beta_min * (-1)
 
-def f_at_point(f, point):
-    f_point = lambdify(x_array[0:len(point)], f, modules='numpy')(*point)
-    return round(f_point, 5)
+def f_at_point(f, point, isRound = False):
+    try:
+        f_point = lambdify(x_array[0:len(point)], f, modules='numpy')(*point)
+    except OverflowError:
+        return float('inf')
+    return round(f_point, 5) if isRound == True else f_point
 
 def printInfo(f, x0, eps, ExpectedRes, ActualResFourCGM, ActualResThreeCGM):
     test_f = "Test function: " + str(f)
@@ -64,16 +79,19 @@ def printInfo(f, x0, eps, ExpectedRes, ActualResFourCGM, ActualResThreeCGM):
     acFourRes = "" if len(ActualResFourCGM) == 0 else "Actual Result 4 steps CGM: x* = " + str(ActualResFourCGM['x_star']) + " f* = " + str(ActualResFourCGM['f_star']) + " for k = %d" %(ActualResFourCGM['k']) + " steps"
     acThreeRes = "" if len(ActualResThreeCGM) == 0 else "Actual Result 3 steps CGM: x* = " + str(ActualResThreeCGM['x_star']) + " f* = " + str(ActualResThreeCGM['f_star']) + " for k = %d" %(ActualResThreeCGM['k']) + " steps"
     breakLine = "\n"
-    print(test_f + breakLine + test_point + breakLine + accuracy + breakLine + exRes + breakLine + acFourRes + breakLine + acThreeRes + breakLine)
+    f_at_init_point = "Function at initial point: " + str(f_at_point(f, x0, True))
+    print(test_f + breakLine + test_point + breakLine + f_at_init_point + breakLine + accuracy + breakLine + exRes + breakLine + acFourRes + breakLine + acThreeRes + breakLine)
 
-def showPlot(f, x_star, fourStepsRes, threeStepsRes):
-    x_star = x_star[1] if type(x_star) == list and type(x_star[1]) == list else x_star
-    f_x_star = f_at_point(f, x_star)
+def showPlot(f, fourStepsRes, threeStepsRes):
+    f_x_star_fourSteps = f_at_point(f, fourStepsRes['x_star'])
+    f_x_star_threeSteps = f_at_point(f, threeStepsRes['x_star'])
+    
     fourSteps_f_points = [f_at_point(f, x_i) for x_i in fourStepsRes['x_points']]
     threeSteps_f_points = [f_at_point(f, x_i) for x_i in threeStepsRes['x_points']]
-    log_fourSteps = [Decimal(f_point_k).ln() - f_x_star for f_point_k in fourSteps_f_points]
-    log_threeSteps = [Decimal(f_point_k).ln() - f_x_star for f_point_k in threeSteps_f_points]
-    
+    log_fourSteps = [Decimal(f_point_k - f_x_star_fourSteps).log10() for f_point_k in fourSteps_f_points]
+    log_threeSteps = [Decimal(f_point_k - f_x_star_threeSteps).log10() for f_point_k in threeSteps_f_points]
+
     plt.plot(range(fourStepsRes['k'] + 1), log_fourSteps)
     plt.plot(range(threeStepsRes['k'] + 1), log_threeSteps)
+    
     plt.show()
